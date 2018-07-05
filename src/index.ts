ce2b8f8b5a7c4ab6c0761bce1d1fad8f4a6aeb3d
Gusejnov Muslim <gusejnov1999@list.ru>
2018-07-05 21:20:25 +0300

    @update(GraphqlServer) update prisma defs

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
-
-const resolvers = {
-  Query: {
-    feed(parent, args, ctx, info) {
-      return ctx.db.query.posts({ where: { isPublished: true } }, info)
-    },
-    drafts(parent, args, ctx, info) {
-      return ctx.db.query.posts({ where: { isPublished: false } }, info)
-    },
-    post(parent, { id }, ctx, info) {
-      return ctx.db.query.post({ where: { id } }, info)
-    },
-  },
-  Mutation: {
-    createDraft(parent, { title, text }, ctx, info) {
-      return ctx.db.mutation.createPost(
-        {
-          data: {
-            title,
-            text,
-          },
-        },
-        info,
-      )
-    },
-    deletePost(parent, { id }, ctx, info) {
-      return ctx.db.mutation.deletePost({ where: { id } }, info)
-    },
-    publish(parent, { id }, ctx, info) {
-      return ctx.db.mutation.updatePost(
-        {
-          where: { id },
-          data: { isPublished: true },
-        },
-        info,
-      )
-    },
-  },
-}
+import resolvers from './resolvers'
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -46,6 +8,7 @@ const server = new GraphQLServer({
   context: req => ({
     ...req,
     db: new Prisma({
+      typeDefs: './src/schema.graphql',
       endpoint: 'https://eu1.prisma.sh/muslim-guseinov-4235e0/ecommerce/ecommerce-dev', // the endpoint of the Prisma API
       debug: true, // log all GraphQL queries & mutations sent to the Prisma API
       // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
