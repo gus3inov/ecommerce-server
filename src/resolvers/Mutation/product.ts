e0f7479a0ca6c937896da7e48b6f4a53df4a4e35
Gusejnov Muslim <gusejnov1999@list.ru>
2018-07-09 23:35:57 +0300

    @create(product.ts) product mutations

---
+++ src/resolvers/Mutation/product.ts
@@ -0,0 +1,42 @@
+const shortid = require("shortid")
+import { createWriteStream } from 'fs'
+import { getUserId, Context } from '../../utils'
+
+const storeUpload = async ({ stream, filename }): Promise<any> => {
+  const id = shortid.generate()
+  const path = `images/${id}`
+
+  return new Promise((resolve, reject) =>
+    stream
+      .pipe(createWriteStream(path))
+      .on('finish', () => resolve({ path }))
+      .on('error', reject),
+  )
+}
+
+const processUpload = async upload => {
+  const { stream, filename, mimetype, encoding } = await upload
+  const { path } = await storeUpload({ stream, filename })
+  return path
+}
+
+export const product = {
+  async createProduct(parent, { name, price, picture }, ctx: Context, info) {
+    const userId = getUserId(ctx)
+    const pictureUrl: string = await processUpload(picture);
+    console.log(userId)
+    return ctx.db.mutation.createProduct(
+      {
+        data: {
+            name,
+            price,
+            pictureUrl,
+            seller: {
+                connect: { id: userId },
+            },
+        },
+      },
+      info
+    )
+  },
+}
