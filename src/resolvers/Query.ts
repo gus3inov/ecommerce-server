2597915708e4c94997b9c38bc31fd46a2038201a
Gusejnov Muslim <gusejnov1999@list.ru>
2018-07-10 23:37:07 +0300

    @update(Query resolver) adding products query

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,6 +1,9 @@
+import { forwardTo } from 'prisma-binding'
 import { getUserId, Context } from '../utils'
+import { product } from './Mutation/product';
 
 export const Query = {
+  products: forwardTo('db'),
   feed(parent, args, ctx: Context, info) {
     return ctx.db.query.posts({ where: { isPublished: true } }, info)
   },
