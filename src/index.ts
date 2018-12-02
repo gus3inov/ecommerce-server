cabf1830819b9edaf73211965198b601dc57a11c
Gusejnov Muslim <gusejnov1999@list.ru>
2018-12-02 12:32:27 +0300

    feature(WebSocket): adding websocket connection

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
-import {
-  Prisma
-} from 'prisma-binding';
+import { createServer } from "http";
+import { Prisma } from "prisma-binding";
 import * as express from "express";
-import {
-  ApolloServer,
-  makeExecutableSchema,
-  gql
-} from "apollo-server-express";
-import resolvers from './resolvers';
-import {
-  importSchema
-} from 'graphql-import';
-import {
-  graphqlUploadExpress
-} from 'graphql-upload'
+import { ApolloServer, makeExecutableSchema } from "apollo-server-express";
+import resolvers from "./resolvers";
+import { importSchema } from "graphql-import";
+import { graphqlUploadExpress } from "graphql-upload";
 
-const typeDefs: any = importSchema('./src/schema.graphql');
+const PORT = 4000;
+const typeDefs: any = importSchema("./src/schema.graphql");
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers
@@ -27,20 +19,23 @@ const server = new ApolloServer({
   context: req => ({
     ...req,
     db: new Prisma({
-      typeDefs: './src/generated/prisma.graphql',
-      endpoint: 'https://eu1.prisma.sh/muslim-guseinov-4235e0/ecommerce/ecommerce-dev',
+      typeDefs: "./src/generated/prisma.graphql",
+      endpoint:
+        "https://eu1.prisma.sh/muslim-guseinov-4235e0/ecommerce/ecommerce-dev",
       debug: true,
-      secret: 'mysecret123',
-    }),
-  }),
-})
+      secret: "mysecret123"
+    })
+  })
+});
 
 const app = express();
 
-app.use(graphqlUploadExpress({
-  maxFileSize: 10000000,
-  maxFiles: 20,
-}))
+app.use(
+  graphqlUploadExpress({
+    maxFileSize: 10000000,
+    maxFiles: 20
+  })
+);
 
 app.use("/images", express.static("images"));
 
@@ -48,8 +43,21 @@ server.applyMiddleware({
   app
 });
 
-app.listen({
-    port: 4000
-  }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-)
+const httpServer = createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(
+  {
+    port: PORT
+  },
+  () => {
+    console.log(
+      `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+    );
+    console.log(
+      `🚀 Subscriptions ready at ws://localhost:${PORT}${
+        server.subscriptionsPath
+      }`
+    );
+  }
+);
